372f82434e02388e3ae2efa1820f9cf769c92d2f
Tomi <tomasorejas1@gmail.com>
2025-07-16 11:36:20 +0200

    Primer HolaMundo

---
+++ holaMundoSolución/holaMundoProyecto/Program.cs
@@ -0,0 +1,18 @@
+﻿internal class Program
+{
+    private static void Main(string[] args)
+    {
+        Console.WriteLine("Hello, World!");
+        Console.WriteLine("===========¿");
+
+        Console.WriteLine(@"     .\^/.          ");
+        Console.WriteLine(@"   . |`|/| .         ");
+        Console.WriteLine(@"  |\|\|'|/|         ");
+        Console.WriteLine(@".--'-\`|/-''--.      ");
+        Console.WriteLine(@" \`-._\|./.-'/       ");
+        Console.WriteLine(@"  >`-._|/.-'<         ");
+        Console.WriteLine(@" '~|/~~|~~\|~'       ");
+        Console.WriteLine(@"       |              ");
+      
+    }
+}
